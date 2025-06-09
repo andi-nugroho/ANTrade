0c6b3209b2c337c32120e724a012548147440f58
NERD777 <nugrohoandi2005@gmail.com>
2025-06-09 13:33:18 +0700

    feat: improve my life

---
+++ farmint-fe/lib/farcaster.ts
@@ -0,0 +1,6 @@
+export const farcasterConfig = {
+  relay: "https://relay.farcaster.xyz",
+  rpcUrl: "https://base-sepolia.drpc.org",
+  domain: "far-trade.vercel.app",
+  siweUri: "https://far-trade.vercel.app",
+};
